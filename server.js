0dfcaa8a82643dbb9a5e9dfffa9f440dfdd4f657
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2018-11-08 09:29:53 +0000

    fiing url

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const schema = require("./schema");
 const resolvers = require("./resolvers");
 
 // Import configuration and connect to DB
-console.log(dbURL);
+// console.log(dbURL);
 const { dbURL, dbName } = require("./config");
-mongoose.connect(dbURL + "/" + dbName);
+mongoose.connect(
+  "mongodb://heroku_09clv9w4:umbj5436ut2mm3prek019135md@ds155663.mlab.com:55663/heroku_09clv9w4/" +
+    dbName
+);
 
 // Define "context" just for testing
 const context = {
