660cfacf558e641eb478aee6317da1aa9b910478
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2018-11-10 18:33:38 +0000

    heroku config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const resolvers = require("./resolvers");
 
 // Import configuration and connect to DB
 const { dbURL, dbName } = require("./config");
-mongoose.connect(dbURL + "/" + dbName);
+// mongoose.connect(dbURL + "/" + dbName);
 mongoose.connect(
   "mongodb://heroku_09clv9w4:umbj5436ut2mm3prek019135md@ds155663.mlab.com:55663/heroku_09clv9w4"
 );
