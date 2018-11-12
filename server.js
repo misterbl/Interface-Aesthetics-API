3ad03f36b840f7b064c44e7a3d3de997c40a1193
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2018-11-12 15:07:20 +0000

    change mongoose.connect to heroku

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const bodyParser = require("body-parser");
 // Import configuration and connect to DB
 const { dbURL, dbName } = require("./config");
 
+// mongoose.connect(
+//   dbURL + "/" + dbName,
+//   { useNewUrlParser: true }
+// );
 mongoose.connect(
-  dbURL + "/" + dbName,
+  "mongodb://heroku_09clv9w4:umbj5436ut2mm3prek019135md@ds155663.mlab.com:55663/heroku_09clv9w4",
   { useNewUrlParser: true }
 );
-// mongoose.connect(
-//   "mongodb://heroku_09clv9w4:umbj5436ut2mm3prek019135md@ds155663.mlab.com:55663/heroku_09clv9w4"
-// );
 // Define "context" just for testing
 const context = {
   greeting: "Hello world!"
